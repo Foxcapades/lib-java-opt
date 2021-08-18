36a59da07bfd8eaaf99681413b2bc529cfef18ad
Elizabeth <foxcapade@gmail.com>
2021-08-18 17:14:08 -0400

    update build to use new nexus url

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ publishing {
   repositories {
     maven {
       name = "nexus"
-      url  = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+      url  = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
 
       credentials {
         username = project.findProperty("nexus.user") as String?
