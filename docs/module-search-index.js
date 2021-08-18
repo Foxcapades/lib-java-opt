31d3bb2b87b703f26a71856d03c8ad60ae0fc286
Elizabeth <foxcapade@gmail.com>
2021-08-18 16:30:37 -0400

    add javadoc

---
+++ docs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"foxcapades.lib.opt"}];updateSearchResults();
---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"foxcapades.lib.opt","l":"io.foxcapades.lib.opt"},{"m":"foxcapades.lib.opt","l":"io.foxcapades.lib.opt.impl"}];updateSearchResults();
